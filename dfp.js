513c701b3fe7fcb4ec7924bdf71b30b5dcfb6801
ajiva-ada <aaisha.jiva@ada.ac.uk>
2024-10-23 14:45:59 +0000

    final commit

--- dfp.js
+++ dfp.js
@@ -1,36 +1,37 @@
 const fs = require('fs');
 
 function parseFile (indata, outdata, delimiter = ';') {
-  // Checks if input file exists #T05
+  // Checks if input file exists (T05 test)
   if (!fs.existsSync(indata)) {
     return -1;
   }
 
-  const data = fs.readFileSync(indata, 'utf-8');
+  const data = fs.readFileSync(indata, 'utf-8'); // read input file
   const lines = data.split(/\n/);
 
-  // avoid repeatdely appending by clearing output file first
+  // clear to avoid appending to old data
   if (fs.existsSync(outdata)) {
     fs.unlinkSync(outdata);
   }
 
   let count = 0;
 
-  const correctLines = lines.slice(1); // remove first line 'header' to correct count
+  const correctLines = lines.slice(1); // remove first line (header) to correct count
 
-  const reviewLines = correctLines.map(line => {
-    const elements = line.split(delimiter); // split by delimiter
+  // map through lines and split by delimiter
+  const reviewLines = correctLines.map(line => { 
+    const elements = line.split(delimiter);
     if (elements.length < 2) return ''; // skipping invalid lines 
 
     const review = elements[0].trim().slice(0, 20); // trim to first 20 characters
     const sentiment = elements[1].trim()
 
-    const newData = `${sentiment}${delimiter}${review}\n`
+    const newData = `${sentiment}${delimiter}${review}\n` // new format for output file
 
-    fs.appendFileSync(outdata, newData) // writing to output file
-    count++ // record count
+    fs.appendFileSync(outdata, newData) // write to output file
+    count++ 
   });
-  return count; // no. records #T02
+  return count; // no. records (T02 test)
   }
 
 // Leave this code here for the automated tests
