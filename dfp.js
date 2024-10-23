84adf249f67038d29863ff44d38d20bd9963f1c3
ajiva-ada <aaisha.jiva@ada.ac.uk>
2024-10-23 11:25:49 +0000

    remove 'header' to correct count pass 6 tests

--- dfp.js
+++ dfp.js
@@ -5,44 +5,52 @@ const fs = require('fs');
 //     fs.unlinkSync(outputFile);
 //   }
 // }
-
 function parseFile (indata, outdata, delimiter = ';') {
   // Checks if input file exists #T05
   if (!fs.existsSync(indata)) {
     return -1;
   }
-  let count = 0;
+
   const data = fs.readFileSync(indata, 'utf-8');
   const lines = data.split(/\n/);
 
-  const review = [];
-  const sentiment = [];
+  //const review = [];
+  //const sentiment = [];
 
-  const reviewLines = lines.map(line => {
-    const elements = line.split(delimiter);
-    if (elements.length < 2) return '';
+  // avoid repeatdely appending by clearing output file first
+  if (fs.existsSync(outdata)) {
+    fs.unlinkSync(outdata);
+  }
 
-    const review = elements[0].trim().slice(0, 20); // Trim review to 20 characters
+  count = 0;
+
+  const correctLines = lines.slice(1); // remove first line 'header' to correct count
+
+  const reviewLines = correctLines.map(line => {
+    const elements = line.split(delimiter); // split by delimiter
+    if (elements.length < 2) return ''; // skipping invalid lines 
+
+    const review = elements[0].trim().slice(0, 20); // trim to first 20 characters
     const sentiment = elements[1].trim()
 
     let newData = `${sentiment};${review}\n`
 
-    fs.appendFileSync(outdata, newData)
+    fs.appendFileSync(outdata, newData) // writing to output file
+    count++ // record count
     //return `${sentiment}${delimiter}${review}`;
-  }).filter(line => line !== ''); // filtering white space
+  })
+  return count; // no. records #T02
+  //.filter(line => line !== ''); // filtering white space
+  }
 
-  
+// Leave this code here for the automated tests
+module.exports = {
+  parseFile,
+}
 
-   /* if (sentiment === 'positive') {
-      positiveElements.push(review);
-    } else {
-      negativeElements.push(review);
-    }
-      */
-  };
+parseFile("./datafile.csv", "./outputfile.csv")
 
-
-  // Original code
+ // Original code
   /*
   for (let i = 0; i < lines.length; i++) {
     const elements = lines[i].split(delimiter);
@@ -56,18 +64,3 @@ function parseFile (indata, outdata, delimiter = ';') {
     }
   }
 */
-
-  
-
-
-
-  
-
-
-
-// Leave this code here for the automated tests
-module.exports = {
-  parseFile,
-}
-
-parseFile("./datafile.csv", "./outputfile.csv")
